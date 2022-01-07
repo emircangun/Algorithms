bab70212826fa21cefc28e12bc83b507953556ab
Emircan Gündoğdu <58917386+emircangun@users.noreply.github.com>
2022-01-07 23:20:11 +0300

    Linked list cycle detector

---
+++ linked_list_cycle_detector.cpp
@@ -0,0 +1,17 @@
+bool hasCycle(ListNode *head)
+{
+    if (head == NULL)
+        return false;
+    
+    ListNode *slow = head, *fast = head->next;
+    while (slow != fast)
+    {
+        if (fast == NULL || fast->next == NULL)
+            return false;
+        
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    
+    return true;
+}
