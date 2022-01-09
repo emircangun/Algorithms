158612d0078a4273b6f616f6efee2b2b4e46732d
Ayca Gökdağ <77059420+aycagokdag@users.noreply.github.com>
2022-01-09 11:36:02 +0300

    Heapify for max heap

--- Heaps/heapify.cpp
+++ Heaps/heapify.cpp
@@ -1,4 +1,5 @@
-void heapify(int arr[], int n, int i){
+//heapify the last element in a max heap
+void up_heapify(int arr[], int n, int i){
   int largest = i; // i is the index of the root
   l = i * 2 + 1; // l is the index of the left child
   r = i * 2 + 2; // l is the index of the right child
@@ -6,16 +7,18 @@ void heapify(int arr[], int n, int i){
   if(l < n && arr[l] > arr[i]){ // n is the size of the array
     largest = l;
   }
-  if(r < n && arr[r] > arr[i]){
+  if(r < n && arr[r] > arr[largest]){
     largest = r;
   }
   
   if(largest != i){
     swap(arr[largest], arr[i]);
-    heapify(arr, n, i);
+    heapify(arr, n, largest);
   }
 }
 
+// max heap 
+// root switched with last element, heapify it downwards
 void down_heapify(int arr[], int n, int i){
   int min = i; // i is the index of the root
   l = i * 2 + 1; // l is the index of the left child
@@ -24,12 +27,12 @@ void down_heapify(int arr[], int n, int i){
   if(l < n && arr[l] < arr[i]){ // n is the size of the array
     min = l;
   }
-  if(r < n && arr[r] < arr[i]){
+  if(r < n && arr[r] < arr[min]){
     min = r;
   }
   
   if(min != i){
     swap(arr[min], arr[i]);
-    heapify(arr, n, i);
+    heapify(arr, n, min);
   }
 }
