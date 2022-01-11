688acc990248e7aa330184d642660310e2e732ab
Emircan Gündoğdu <58917386+emircangun@users.noreply.github.com>
2022-01-11 23:35:35 +0300

    Is binary tree is symmetric

---
+++ is_tree_symmetric.cpp
@@ -0,0 +1,15 @@
+TreeNode* leftRight(TreeNode* root)
+{
+    if (root == NULL)
+        return NULL;
+    
+    TreeNode *left, *right;
+    left = leftRight(root->left);
+    right = leftRight(root->right);
+    
+    return root;
+}
+
+bool isSymmetric(TreeNode* root) {
+    
+}
