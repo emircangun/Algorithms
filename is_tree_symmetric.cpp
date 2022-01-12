38632cdfd6f373bbefd8e2e7f299fbc2f1b98e1e
Emircan Gündoğdu <58917386+emircangun@users.noreply.github.com>
2022-01-13 00:00:50 +0300

    Update is tree symmetric

--- is_tree_symmetric.cpp
+++ is_tree_symmetric.cpp
@@ -1,15 +1,35 @@
-TreeNode* leftRight(TreeNode* root)
+queue<int> s;
+    
+void leftPushPreorder(TreeNode* root)
 {
     if (root == NULL)
-        return NULL;
+        return;
     
-    TreeNode *left, *right;
-    left = leftRight(root->left);
-    right = leftRight(root->right);
+    s.push(root->val);
+    leftPushPreorder(root->left);
+    leftPushPreorder(root->right);
+}
+
+void rightPopPreorder(TreeNode* root)
+{
+    if (root == NULL)
+    {
+        if (s.front();
+        s.pop();
+        return;
+    }
+    int leftSubtree = s.front();
+    s.pop();
+    if (leftSubtree != root->val)
+        return;
     
-    return root;
+    rightPopPreorder(root->right);
+    rightPopPreorder(root->left);
+    
 }
 
 bool isSymmetric(TreeNode* root) {
-    .
+    leftPushPreorder(root);
+    rightPopPreorder(root);
+    return (s.empty());
 }
