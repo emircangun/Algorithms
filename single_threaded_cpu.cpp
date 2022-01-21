d97911651841845dba675e54850f975a96486c0f
Emircan Gündoğdu <58917386+emircangun@users.noreply.github.com>
2022-01-21 23:28:21 +0300

    Single threaded CPU with a priority queue

--- single_threaded_cpu.cpp
+++ single_threaded_cpu.cpp
@@ -1,19 +1,55 @@
-bool CompareDist(pair<int,int>, pair<int,int> b)
+class Task
 {
-    return a.second > b.second;
-}
-vector<int> getOrder(vector<vector<int>>& tasks)
-{
-    vector<int> res;
-    int i = 1;
-    priority_queue<pair<int,int>,vector<pair<int,int>>,CompareDist> pq;
-    pq.push(make_pair(tasks[0][0], tasks[0][1]));
-    int time = tasks[0][0];
-    while (i <= tasks.size())
+public:
+    int index;
+    int startingTime;
+    int processingTime;
+    Task(int _index, int _startingTime, int _processingTime)
     {
-        if (tasks[i][0] == time)
+        index = _index;
+        startingTime = _startingTime;
+        processingTime = _processingTime;
+    }
+};
+
+class TaskComparator 
+{
+public:
+    bool operator()(const Task& t1, const Task& t2)
+    {
+        return (t1.processingTime > t2.processingTime);
+    }
+};
+
+
+vector<int> getOrder(vector<vector<int>>& tasks) {
+    vector<int> res;
+    priority_queue<Task, vector<Task>, TaskComparator> pq;
+    pq.push(Task(0, tasks[0][0], tasks[0][1]));
+    
+    int time = tasks[0][0];
+    int i = 1;
+    while (i < tasks.size() || !pq.empty())
+    {
+        if (i < tasks.size() && tasks[i][0] <= time)
         {
-            pq.push(make_pair(tasks[i][0], tasks[i][1]));
+            pq.push(Task(i, tasks[i][0], tasks[i][1]));
+            i++;
+            continue;
+        }
+        
+        if (!pq.empty())
+        {
+            Task nextTask = pq.top();
+            pq.pop();
+            res.push_back(nextTask.index);
+            time += nextTask.processingTime;
+        }
+        else
+        {
+            time++;
         }
     }
+    
+    return res;
 }
