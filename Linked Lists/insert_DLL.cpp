ed4b42eca323edfdf65f5abbf0bde0ff599a4e9d
Ayca Gökdağ <77059420+aycagokdag@users.noreply.github.com>
2022-01-23 23:54:14 +0300

    Insert at DLL

--- Linked Lists/insert_DLL.cpp
+++ Linked Lists/insert_DLL.cpp
@@ -1,1 +1,20 @@
 
+DoublyLinkedListNode* sortedInsert(DoublyLinkedListNode* llist, int data) {
+    DoublyLinkedListNode *temp, *temp2= llist; 
+    DoublyLinkedListNode *node = new DoublyLinkedListNode(data);
+    if (llist == NULL){
+        llist = node;
+        return llist;
+    }
+    while (temp->next != NULL && temp->next->data < data) {
+        temp = temp->next;
+    }
+
+    temp2 = temp->next;
+    temp->next = node;
+    node->next = temp2;
+    node->prev = temp;
+    node->next->prev = node;
+
+    return llist;
+}
