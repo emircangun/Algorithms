230e6f2a7ef70114882f03fd6289f444f70db845
Ayca Gökdağ <77059420+aycagokdag@users.noreply.github.com>
2022-02-09 13:03:17 +0300

    Add Two Sum II

--- two_sum.cpp
+++ two_sum.cpp
@@ -11,3 +11,20 @@ vector<int> twoSum(vector<int>& nums, int target) {
     
     return vector<int>{};
 } 
+
+// TWO SUM II
+// if array is sorted
+// O(n) time complexity
+
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int l = 0;
+        int r = numbers.size() - 1;
+        
+        while(r > l){
+            if(numbers[l] + numbers[r] > target) r--;
+            else if(numbers[l] + numbers[r] < target) l++;
+            else return vector<int>{l + 1, r + 1}; // oddly indexes start from 1 in the example on leetcode
+        }
+        
+        return vector<int>{};
+    }
